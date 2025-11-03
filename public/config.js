c429ad8edf5ca39233caefaf816efe92a32d9eeb
indahPurnamaSarii <indahpurnm1@gmai.com>
2025-11-03 22:03:43 +0900

    Perbaiki .gitignore dan tambahkan config.js publik

---
+++ public/config.js
@@ -0,0 +1,14 @@
+// Isi file: config.js
+
+// Inisialisasi Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSy...KUNCI_PUBLIK_ANDA...",
+  authDomain: "papaya-marzipan-d111ed.firebaseapp.com",
+  projectId: "papaya-marzipan-d111ed",
+  storageBucket: "papaya-marzipan-d111ed.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
+};
+
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
